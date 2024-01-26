630dc2ab9f74ade4e62ecbeb6cb19238439fec21
George Ungureanu Vranceanu <george.ungureanuvranceanu@imc.com>
2024-01-26 13:58:06 +0100

    Team 3 Solution

---
+++ Hackathon/Solution/app/deploy.sh
@@ -0,0 +1,3 @@
+kubectl apply -f hpa.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f svc.yaml
---
+++ Hackathon/Solution/mongodb/deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f secrets.yaml
+kubectl apply -f statefulset.yaml
+kubectl apply -f stc-svc.yaml
+kubectl apply -f stc-svc-monitor.yaml
+kubectl apply -f exporter-config.yaml
